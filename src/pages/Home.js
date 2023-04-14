a15af782956e3ddc3d8db484e038f2667c0f9a0a
oscarfgmaia <oscarfgmaia@gmail.com>
2023-04-13 21:39:36 -0300

    save: in progress

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 export default function Home() {
-  return <h1>HOME PAGE</h1>;
+  return (
+    <CenteredContainer>
+      <h1>HOME PAGE</h1>
+      <h2>APRESENTAÇÃO SOBRE O SITE</h2>
+      <Link to="/sign-in"> ir para login page</Link>
+      <Link to="/sign-up"> cadastre-se </Link>
+    </CenteredContainer>
+  );
 }
+
+const CenteredContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  a:hover {
+    background-color: red;
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+  }
+  a:visited{
+    color: inherit;
+  }
+`;
